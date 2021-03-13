ccf31ea73cd8faf0e9d4e2a347a80ab18a1f5f42
Bharat Dave <davebharat93@gmail.com>
2021-03-13 18:54:44 -0500

    Changed Sargent to Thaddeus Ross

--- src/avengersProfile.js
+++ src/avengersProfile.js
@@ -2,43 +2,31 @@ export const avengersProfile =[
 	{
 		name:"Tony Stark",
 		heroName:"Iron Man",
-		imageAddress:"./Iron_Man.jpg",
-		//connections:"Howard Stark,Maria Stark,Pepper Potts"
 		connections:{0:"Pepper Potts",1:"Howard Stark",2:"Maria Stark"}
 	},
 	{
 		name:"Steve Rogers",
 		heroName:"Captain America",
-		imageAdress:"./Captain_America.jpg",
-		//connections:"Bucky Barnes,Peggy Carter"
 		connections:{0:"James Barnes",1:"Margaret Carter"}
 	},
 	{
 		name:"Thor Odinson",
 		heroName:"Thor",
-		imageAdress:"./Thor.jpg",
-		//connections:"Bucky Barnes,Peggy Carter"
 		connections:{0:"Jane Foster",1:"Loki"}
 	},
 	{
 		name:"Bruce Banner",
 		heroName:"Hulk",
-		imageAddress:"./Hulk.jpg",
-		//connections:"Betty Ross,Sgnt. Ross"
-		connections:{0:"Betty Ross",1:"Sgnt Ross"}
+		connections:{0:"Betty Ross",1:"Thaddeus Ross"}
 	},
 	{
 		name:"Natasha Romanoff",
 		heroName:"Black Widow",
-		imageAddress:"./Black_Widow.jpg",
-		//connections:"Nick Fury"
 		connections:{0:"Nick Fury"}
 	},
 	{
 		name:"Clint Barton",
 		heroName:"Hawkeye",
-		imageAddress:"./Hawkeye.png",
-		//connections:"Laura Barton"
 		connections:{0:"Laura Barton"}
 	}
 ];
